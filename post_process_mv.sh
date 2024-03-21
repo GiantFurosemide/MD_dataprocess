e9526f59ecb601641c7782dd697c7374c99ccabd
MuWang <18640057101@163.com>
2024-03-21 20:51:59 +0800

    fix bug

--- post_process_mv.sh
+++ post_process_mv.sh
@@ -4,6 +4,7 @@ mkdir -p '../wangmu_check'
 mkdir -p ${MY_OUT_DIR}
 mkdir -p ${MY_OUT_DIR}/coor_nc
 mkdir -p ${MY_OUT_DIR}/log_txt
-mv -v ./*/*.trr ${MY_OUT_DIR}/coor_nc && mv -v ./*/*log.txt ${MY_OUT_DIR}/log_txt
+mv -v ./*/*.trr ${MY_OUT_DIR}/coor_nc 
+mv -v ./*/*log.txt ${MY_OUT_DIR}/log_txt
 echo '\n > Done!'
 cd coor_nc
