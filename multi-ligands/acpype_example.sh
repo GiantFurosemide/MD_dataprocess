591d6a9f25e2c8ad6c350b3facb50914ae20201a
MuWang <18640057101@163.com>
2024-03-27 10:13:21 +0800

    update ligand building process

--- multi-ligands/acpype_example.sh
+++ multi-ligands/acpype_example.sh
@@ -6,6 +6,29 @@ acpype -i DAS.mol2 -f
 obabel -ipdb DAS-jligand.pdb -omol -h > DAS.sdf  
 acpype -i DAS.sdf -f
 
-# acpype by smile, 
+# acpype by smile,
 acpype -i 'CCCC' -b 'DAS'
 acpype -i "$(cat smi/dasatinib.smi)" -b 'DAS'
+
+# obabel gen3d maybe generate error 3D coordinates
+obabel -:"SCCCCCCCCCCCO" -omol2 -h --gen3d > smiles2mol_example_smiles_1.mol2 
+
+# use acedrg to generate 3D coordinates
+# -i input.smile
+# -o output name GA.pdb
+# 3-letter code is GAC
+# will generate GA.pdb in current directory
+
+file_in_smile="input.smile"
+output_name="GA"
+output_3letter_code="GAC"
+acedrg -i $file_in_smile -o $output_name -r $output_3letter_code
+obabel -ipdb ${output_name}.pdb -omol2  > ${output_name}.mol2
+acpype -i ${output_name}.mol2
+
+file_in_sdf="input.sdf"
+output_name="GA"
+output_3letter_code="GAC"
+acedrg -i $file_in_sdf -o $output_name -r $output_3letter_code
+obabel -ipdb ${output_name}.pdb -omol2  > ${output_name}.mol2
+acpype -i ${output_name}.mol2
