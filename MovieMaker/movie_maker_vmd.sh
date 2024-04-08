8b91c0f3e5b9c673f3bd4ed3381d514441151110
MuWang <18640057101@163.com>
2024-04-08 23:16:13 +0800

    update

--- MovieMaker/movie_maker_vmd.sh
+++ MovieMaker/movie_maker_vmd.sh
@@ -9,4 +9,5 @@
 # time duration = frames / frame rate
 # example: 251 frames, 25 frames per second, 251/25=10.04s movies will be generated
 out_movie_name='3imn_CYP'
-ffmpeg -an -i ./${out_movie_name}.%05d.ppm -vcodec mpeg2video -r 25 -c:v libx264 -crf 0 -vframes 251 ${out_movie_name}_r25.mov
+ffmpeg -an -i ./${out_movie_name}.%05d.ppm -vcodec mpeg2video -r 25 -c:v libx264 -crf 0  ${out_movie_name}_r25.mov
+ffmpeg -an -i ./${out_movie_name}.%05d.ppm -vcodec mpeg2video -r 25 -c:v libx264 -crf 0  ${out_movie_name}_r25.mp4
