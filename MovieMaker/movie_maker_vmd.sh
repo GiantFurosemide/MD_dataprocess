e83e3b9a82f0acfef60778f41a46f0e9052b6ac2
MuWang <18640057101@163.com>
2024-04-08 22:36:58 +0800

    update movies maker by vmd

---
+++ MovieMaker/movie_maker_vmd.sh
@@ -0,0 +1,12 @@
+# 0. generate ppm files by in VMD
+#
+# Renderer:Select Snapshot(screen capture)
+# Movie settings: select 'trajectory'; unselect 'delete image files'
+# Format: no matter what to select, we will use another command
+# set out_movie_name 
+
+# 1. merge ppm files to one movie by ffmpeg
+# time duration = frames / frame rate
+# example: 251 frames, 25 frames per second, 251/25=10.04s movies will be generated
+out_movie_name='3imn_CYP'
+ffmpeg -an -i ./${out_movie_name}.%05d.ppm -vcodec mpeg2video -r 25 -c:v libx264 -crf 0 -vframes 251 ${out_movie_name}_r25.mov
