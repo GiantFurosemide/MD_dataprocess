ae21d7f70e03c25a43f7fdab49cf8cd7b57520f8
MuWang <18640057101@163.com>
2024-04-15 15:49:24 +0800

    update movies maker

--- MovieMaker/movie_maker_vmd.sh
+++ MovieMaker/movie_maker_vmd.sh
@@ -14,6 +14,7 @@
 
 # (recommanded)
 # decoder 'prores' for macos and ubuntu
+# -csf [0-51]: 0 lossless 23 default 51 worst quality
 out_movie_name='3imn_CYP'
 ffmpeg -an -i ./${out_movie_name}.%05d.ppm -r 25 -c:v prores -crf 0  ${out_movie_name}_r25.mov
 #ffmpeg -an -i ./${out_movie_name}.%05d.ppm -r 25 -c:v prores -crf 0  ${out_movie_name}_r25.mp4
