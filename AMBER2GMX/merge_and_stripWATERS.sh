0854a544e8069a6f497fcbf9548a97b63798b90a
MuWang <18640057101@163.com>
2024-05-17 13:52:06 +0800

    update to generate rst7 parm7 without water

--- AMBER2GMX/merge_and_stripWATERS.sh
+++ AMBER2GMX/merge_and_stripWATERS.sh
@@ -6,7 +6,9 @@
 parm_file="charmm2amber.parm7"
 rst7_file="amber.rst7"
 # output files
-coor_all="merge.nc"
+coor_no_water="merge_no_water.nc"
+rst7_no_water="amber_no_water.rst7"
+parm_no_water="charmm2amber_no_water.parm7"
 #vel_all="velocity.nc"
 #out_trr="merge_amber.nc"
 # then run 
@@ -17,9 +19,9 @@ coor_all="merge.nc"
 mkdir coor_nc
 mkdir vel_nc
 mkdir log_txt
-mv ./*/*_coor.nc coor_nc/
-mv ./*/*_vel.nc vel_nc/
-mv ./*/*_log.txt log_txt/
+mv md_post_output/*/*_coor.nc coor_nc/
+mv md_post_output/*/*_vel.nc vel_nc/
+mv md_post_output/*/*_log.txt log_txt/
 
 conda activate AmberTools23
 
@@ -34,6 +36,22 @@ quit
 EOF
 cpptraj -i cpptraj000.in
 
+cat > cpptraj001.in <<EOF
+parm $parm_file
+trajin $rst7_file
+strip :WAT
+trajout ${rst7_no_water}
+run
+quit
+EOF
+cpptraj -i cpptraj001.in
+
+cat > no_water.parmed <<EOF
+loadRestrt ${rst7_no_water}
+outparm ${parm_no_water}
+run
+EOF
+parmed -i no_water.parmed
 
 conda deactivate
 ############################################################################################################
