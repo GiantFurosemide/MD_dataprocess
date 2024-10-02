a3820a862ee97c10784942e25cfabdf81de3dd43
MuWang <18640057101@163.com>
2024-10-02 14:06:46 +0800

    update tpr version for robust  merge and align

--- atom_all_atom_pbc_tpr.sh
+++ atom_all_atom_pbc_tpr.sh
@@ -9,7 +9,7 @@ q
 EOF
 
 # calculate middle atom index
-echo Protein | gmx trjconv -f step4.1_equilibration.gro -s  step4.1_equilibration.gro -o npt_ab_protein.gro 
+echo Protein | gmx trjconv -f step4.1_equilibration.gro -s  step4.1_equilibration.tpr -o npt_ab_protein.gro 
 MIDDLE_LINE_NUMBER=$(($(cat npt_ab_protein.gro | wc -l)/2))
 
 # initilize the value of CENTER_ATOM_NUMBER
@@ -46,11 +46,11 @@ echo "[ center ]" >> index_sel.ndx
 echo "$CENTER_ATOM_NUMBER" >> index_sel.ndx
 # todo: need to pick atom for making index file
 
-echo System | gmx trjconv -f merged.xtc -s  step4.1_equilibration.gro -o whole_all.xtc -pbc whole
-echo center System | gmx trjconv -f whole_all.xtc -s step4.1_equilibration.gro -n index_sel.ndx -o atom_all.xtc -pbc atom  -center
-echo C-alpha C-alpha System | gmx trjconv  -s step4.1_equilibration.gro -f atom_all.xtc -fit rot+trans  -center -o atom_rottrans_all.xtc
+echo System | gmx trjconv -f merged.xtc -s  step4.1_equilibration.tpr -o whole_all.xtc -pbc whole
+echo center System | gmx trjconv -f whole_all.xtc -s step4.1_equilibration.tpr -n index_sel.ndx -o atom_all.xtc -pbc atom  -center
+echo C-alpha C-alpha System | gmx trjconv  -s step4.1_equilibration.tpr -f atom_all.xtc -fit rot+trans  -center -o atom_rottrans_all.xtc
 mv -v atom_rottrans_all.xtc atom_all.xtc
-echo C-alpha C-alpha System | gmx trjconv  -s step4.1_equilibration.gro -f atom_all.xtc -fit rot+trans  -center -o atom_rottrans_all.xtc
+echo C-alpha C-alpha System | gmx trjconv  -s step4.1_equilibration.tpr -f atom_all.xtc -fit rot+trans  -center -o atom_rottrans_all.xtc
 
 rm -rfv whole_all.xtc
 rm -rfv atom_all.xtc
