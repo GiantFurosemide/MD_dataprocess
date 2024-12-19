497007bab6c038863553b771a327e44250a45753
MuWang <18640057101@163.com>
2024-12-19 09:51:00 +0800

    Update mergeAMBERtraj_GPU.py
    fix bug

--- AMBER2GMX/mergeAMBERtraj_GPU.py
+++ AMBER2GMX/mergeAMBERtraj_GPU.py
@@ -11,7 +11,7 @@ cmd = ""
 for i in nc_files:
     cmd += f"trajin {i}\n"
 cpptraj_file_in += cmd
-cpptraj_file_in += "trajout merge.nc \n"
+cpptraj_file_in += "trajout merge.nc netcdf \n"
 
 with open("cpptraj.in","w") as file_out:
     file_out.write(cpptraj_file_in)
